95231a11d1c8a1bbb112b9835da1f9e514898f8c
wangyaoqianw-max <wangyaoqianw@gmail.com>
2026-09-11 17:51:06 +0800

    refactor: adapt S01 board bindings

--- 03_Firmware/Application/OTA_APP/04_Impl/impl_bsp/impl_platform_bsp_gpio.c
+++ 03_Firmware/Application/OTA_APP/04_Impl/impl_bsp/impl_platform_bsp_gpio.c
@@ -22,13 +22,8 @@
 
 //******************************** Constants ********************************//
 static impl_platform_gpio_context_t g_statusLedContext = {
-    LED_OUT_GPIO_Port,
-    LED_OUT_Pin
-};
-
-static impl_platform_gpio_context_t g_userKeyContext = {
-    KEY_IN_GPIO_Port,
-    KEY_IN_Pin
+    LED_1_GPIO_Port,
+    LED_1_Pin
 };
 
 static impl_platform_gpio_context_t g_softI2cSclContext = {
@@ -41,25 +36,6 @@ static impl_platform_gpio_context_t g_softI2cSdaContext = {
     I2C_SDA_Pin
 };
 
-static impl_platform_gpio_context_t g_lcdCsContext = {
-    LCD_CS_GPIO_Port,
-    LCD_CS_Pin
-};
-
-static impl_platform_gpio_context_t g_lcdDcContext = {
-    LCD_DC_GPIO_Port,
-    LCD_DC_Pin
-};
-
-static impl_platform_gpio_context_t g_lcdResetContext = {
-    LCD_RST_GPIO_Port,
-    LCD_RST_Pin
-};
-
-static impl_platform_gpio_context_t g_lcdBacklightContext = {
-    LCD_BL_GPIO_Port,
-    LCD_BL_Pin
-};
 //******************************** Constants ********************************//
 
 //******************************** Functions *********************************//
@@ -73,18 +49,6 @@ platform_error_t platform_bsp_gpio_construct_status_led(
     return impl_platform_gpio_construct(gpio,
                                         "status_led_gpio",
                                         &g_statusLedContext);
-}
-
-platform_error_t platform_bsp_gpio_construct_user_key(
-    platform_gpio_t *gpio)
-{
-    if (gpio == NULL) {
-        return PLATFORM_ERR_INVALID_PARAM;
-    }
-
-    return impl_platform_gpio_construct(gpio,
-                                        "user_key_gpio",
-                                        &g_userKeyContext);
 }
 
 platform_error_t platform_bsp_gpio_construct_soft_i2c_scl(
@@ -111,51 +75,4 @@ platform_error_t platform_bsp_gpio_construct_soft_i2c_sda(
                                         &g_softI2cSdaContext);
 }
 
-platform_error_t platform_bsp_gpio_construct_lcd_cs(
-    platform_gpio_t *gpio)
-{
-    if (gpio == NULL) {
-        return PLATFORM_ERR_INVALID_PARAM;
-    }
-
-    return impl_platform_gpio_construct(gpio,
-                                        "lcd_cs_gpio",
-                                        &g_lcdCsContext);
-}
-
-platform_error_t platform_bsp_gpio_construct_lcd_dc(
-    platform_gpio_t *gpio)
-{
-    if (gpio == NULL) {
-        return PLATFORM_ERR_INVALID_PARAM;
-    }
-
-    return impl_platform_gpio_construct(gpio,
-                                        "lcd_dc_gpio",
-                                        &g_lcdDcContext);
-}
-
-platform_error_t platform_bsp_gpio_construct_lcd_reset(
-    platform_gpio_t *gpio)
-{
-    if (gpio == NULL) {
-        return PLATFORM_ERR_INVALID_PARAM;
-    }
-
-    return impl_platform_gpio_construct(gpio,
-                                        "lcd_reset_gpio",
-                                        &g_lcdResetContext);
-}
-
-platform_error_t platform_bsp_gpio_construct_lcd_backlight(
-    platform_gpio_t *gpio)
-{
-    if (gpio == NULL) {
-        return PLATFORM_ERR_INVALID_PARAM;
-    }
-
-    return impl_platform_gpio_construct(gpio,
-                                        "lcd_backlight_gpio",
-                                        &g_lcdBacklightContext);
-}
 //******************************** Functions *********************************//
